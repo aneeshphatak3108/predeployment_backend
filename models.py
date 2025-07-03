a9ed0c47d10bb832a8c848640c445082297a77c6
Aneesh Phatak <aneesh.m.phatak@gmail.com>
2025-07-03 10:20:21 +0530

    mongo atlas

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
+load_dotenv()
+#client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client['typingtestdb']
 
 users_collection = db['users']
